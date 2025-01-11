578dec981c0d98ca3097f95d48e7351bc35a92ba
Arie Timmerman <arietimmerman@gmail.com>
2025-01-11 09:46:39 +0100

    Improved pagesize and max size support, especially for cusor pagination

--- config/scim.php
+++ config/scim.php
@@ -4,4 +4,9 @@ return [
     "publish_routes" => true,
     'omit_main_schema_in_return' => false,
     'omit_null_values' => true,
+
+    'pagination' => [
+        'defaultPageSize' => 10,
+        'maxPageSize' => 100,
+    ]
 ];

c5beeb760442febfa2273f55bbd3e9d0196994e2
Arie Timmerman <arietimmerman@gmail.com>
2025-03-26 16:25:38 +0100

    Support remove operations by specifying path with a filter

--- src/Attribute/MutableCollection.php
+++ src/Attribute/MutableCollection.php
@@ -31,11 +31,39 @@ class MutableCollection extends Collection
         $object->load($this->attribute);
     }
 
-    public function remove($value, Model &$object, Path $path = null)
+    public function remove($value, Model &$object, ?Path $path = null)
     {
-        $values = collect($value)->pluck('value')->all();
+        if ($path?->getValuePathFilter()?->getComparisonExpression() != null) {
+            $attributes = $path?->getValuePathFilter()?->getComparisonExpression()?->attributePath?->attributeNames;
+            $operator = $path?->getValuePathFilter()?->getComparisonExpression()?->operator;
+            $compareValue = $path?->getValuePathFilter()?->getComparisonExpression()?->compareValue;
+            
+            if ($value != null) {
+                throw new SCIMException('Remove operation with filter requires a null value parameter', 400);
+            }
 
-        $object->{$this->attribute}()->detach($values);
+            if (count($attributes) != 1) {
+                throw new SCIMException(sprintf('Filter must specify exactly one attribute, found %d attributes', count($attributes)), 400);
+            }
+
+            if ($operator != 'eq') {
+                throw new SCIMException(sprintf('Unsupported filter operator "%s" - only "eq" is supported', $operator), 400);
+            }
+
+            if ($attributes[0] != 'value') {
+                throw new SCIMException(sprintf('Cannot filter on "%s" - only filtering on "value" attribute is supported', $attributes[0]), 400);
+            }
+
+            if ($value != null) {
+                throw new SCIMException('Cannot specify both a filter path and a value parameter simultaneously', 400);
+            }
+
+            $object->{$this->attribute}()->detach([$compareValue]);
+        } else {
+            $values = collect($value)->pluck('value')->all();
+
+            $object->{$this->attribute}()->detach($values);
+        }
 
         $object->load($this->attribute);
     }

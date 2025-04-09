34239300fb88a4b27072782aaa12aae0c7274676
Arie Timmerman <arietimmerman@gmail.com>
2025-04-09 18:10:06 +0200

    Set attribute type based on validations used, or fallback to default (#115)
    Closes #114

--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -37,6 +37,33 @@ class SchemaTest extends TestCase
     {
         $response = $this->get('/scim/v2/Schemas');
         $response->assertStatus(200);
+
+        $jsonResponse = $response->json();
+        $this->assertNotEmpty($jsonResponse);
+
+        // Find the User schema
+        $userSchema = null;
+        foreach ($jsonResponse['Resources'] as $schema) {
+            if ($schema['id'] === 'urn:ietf:params:scim:schemas:core:2.0:User') {
+                $userSchema = $schema;
+                break;
+            }
+        }
+
+        $this->assertNotNull($userSchema, "User schema not found");
+
+        // Find the active attribute in the schema attributes
+        $activeAttribute = null;
+        foreach ($userSchema['attributes'] as $attribute) {
+            if ($attribute['name'] === 'active') {
+                $activeAttribute = $attribute;
+                break;
+            }
+        }
+
+        $this->assertNotNull($activeAttribute, "Active attribute not found");
+        $this->assertEquals('boolean', $activeAttribute['type'], "Active attribute is not of type boolean");
+
     }
 
     public function getSchemaGenerator(){
